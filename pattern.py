e4f36fbe66fc9f6c6d754c0100c39df1df852e19
YASH JAIN <yashj7911.gmail.com>
2022-10-30 14:00:40 +0530

    commit pattern file

---
+++ pattern.py
@@ -0,0 +1,72 @@
+# for i in range(6):
+#     for j in range(i):
+#         print("*",end=" ")
+#     print('\n')
+
+# for i in range (6):
+#     for j in range(6-i):
+#         print(" ",end=' ')
+#     for k in range(i+1):
+#         print("*",end=" ")
+#     print('\n')
+
+# for i in range(5):
+#     for j in range(i):
+#         print("*",end=" ")
+#     print('\n')
+# for i in range (5):
+#     for j in range(5-i):
+#         print("*",end=" ")
+#     print('\n')
+
+# for i in range(6):
+#     for j in range(6-i):
+#         print(" ", end=" ")
+#     for k in range(i):
+#         print("*", end=" ")
+#     print('\n')
+# for i in range(6):
+#     for j in range(i):
+#         print(" ", end=" ")
+#     for k in range(6-i):
+#         print("*", end=" ")
+#     print('\n')
+
+# g=1
+# for i in range(6):
+#     for j in range(i):
+#         print(g,end=" ")
+#         g+=1
+#     print('\n')
+
+# for i in range(6):
+#     for j in range(i):
+#         print("*",end=" ")
+#     for k in range(6-1-i):
+#         print(" ", end=" ")
+#     for o in range(6-1-i):
+#         print(" ",end=" ")
+#     for j in range(i):
+#         print("*",end=" ")
+#     print('\n')
+
+for i in range(6):
+    for j in range(i):
+        print("*",end=" ")
+    for k in range(6-1-i):
+        print(" ", end=" ")
+    for o in range(6-1-i):
+        print(" ",end=" ")
+    for j in range(i):
+        print("*",end=" ")
+    print('\n')
+for i in range(6):
+    for j in range(5-i-1):
+        print("*",end=" ")
+    for k in range(i+1):
+        print(" ", end=" ")
+    for o in range(i+1):
+        print(" ",end=" ")
+    for j in range(5-i-1):
+        print("*",end=" ")
+    print('\n')
