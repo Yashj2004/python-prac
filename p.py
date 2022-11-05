ca1a2da9cd609912eb72bb824382f90b47069471
Yashj2004 <116498517+Yashj2004@users.noreply.github.com>
2022-11-05 19:01:11 +0530

    Add files via upload

---
+++ p.py
@@ -0,0 +1,20 @@
+from random import choice , shuffle , randint
+
+l=[2,3,5,6,7,8,9,0]
+print(choice(l))
+
+a=["y","a","b","c"]
+shuffle(a)
+print(a)
+
+b=randint(1,1000)
+print(b)
+
+
+import math
+a=9.9
+print(math.ceil(a))
+print(math.floor(a))
+# b=int(input("enter the number"))
+# print(abs(b))-
+print(math.sqrt(a))
