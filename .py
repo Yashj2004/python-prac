6dcfaae6bd0b278e7f545916ed7833045008a5e4
Yashj2004 <116498517+Yashj2004@users.noreply.github.com>
2022-11-06 16:25:08 +0530

    Add files via upload

---
+++ .py
@@ -0,0 +1,38 @@
+# fuctions 
+# inbuild
+# anonymous 
+# user defined 
+
+# module using function
+# random , maths 
+
+# randam =
+# choice()
+# shuffle 
+# randint 
+
+
+# for random import choice
+# l1=[3,5,2,4,2,4,4,5,6,7,7]
+# print(choice(l1))
+
+for random import
+ 
+#  ceil->up->78.5->79
+# floor->down->78.5->78
+# abs->|-5|==5
+# sqrt->
+# cubrt->
+
+# recursion->factorial 
+def factorial(x):
+    if x==1:
+        return 1
+    else:
+        return(x*factorial(x-1))
+
+num=int(input("enter any number"))
+print(num,factorial(num))
+
+
+
